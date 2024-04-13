5c24225e753dd8102c85e136f3e7329c3ef83cdb
Szabina Korbai <korbai.szabina@gmail.com>
2024-04-13 15:14:00 +0300

    Adding App.jsx and main.jsx after completing exercise 1.2

---
+++ part1/src/App.jsx
@@ -0,0 +1,57 @@
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part} {props.number}
+      </p>
+    </div>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+        <Part part={props.part} number={props.number} />
+      </p>
+    </div>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.ex1 + props.ex2 + props.ex3}</p>
+    </div>
+  );
+};
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part={part1} number={exercises1} />
+      <Content part={part2} number={exercises2} />
+      <Content part={part3} number={exercises3} />
+      <Total ex1={exercises1} ex2={exercises2} ex3={exercises3} />
+    </div>
+  );
+};
+
+export default App;
