f9896b9d63e6291bf819e521b19514f5da9d644b
Szabina Korbai <skorbai@student.hive.fi>
2024-04-25 13:38:27 +0300

    Started working on exercise 1.6

---
+++ part1/unicafe/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+const Button = (props) => {
+  const text = props.text;
+
+  return <button>{text}</button>;
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button text="good" />
+      <Button text="neutral" />
+      <Button text="bad" />
+      <h1>Statistics</h1>
+    </div>
+  );
+};
+
+export default App;
