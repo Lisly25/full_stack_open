2f01d7930f2987597da4b683237ad6cf87964654
Szabina Korbai <korbai.szabina@gmail.com>
2024-05-27 17:05:23 +0300

    Added app version of unicafe

---
+++ part1/unicafe_app/src/App.jsx
@@ -0,0 +1,87 @@
+import { useState } from 'react';
+
+const Button = (props) => {
+  const onClick = props.onClick;
+  const text = props.text;
+
+  return <button onClick={onClick}>{text}</button>;
+};
+
+const StatisticLine = (props) => {
+  const text = props.text;
+  const value = props.value;
+
+  if (text === 'positive') {
+    return (
+      <tr>
+        <td>{text}</td>
+        <td>{value} %</td>
+      </tr>
+    );
+  }
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
+
+const Statistics = (props) => {
+  const good = props.good;
+  const neutral = props.neutral;
+  const bad = props.bad;
+
+  const all = good + bad + neutral;
+  const average = (good * 1 + neutral * 0 + bad * -1) / all;
+  const positivePercent = (good / all) * 100;
+
+  if (good == 0 && neutral == 0 && bad == 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>statistics</h1>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={all} />
+          <StatisticLine text="average" value={average} />
+          <StatisticLine text="positive" value={positivePercent} />
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const addGood = () => setGood(good + 1);
+
+  const addNeutral = () => setNeutral(neutral + 1);
+
+  const addBad = () => setBad(bad + 1);
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={addGood} text="good" />
+      <Button onClick={addNeutral} text="neutral" />
+      <Button onClick={addBad} text="bad" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+};
+
+export default App;
