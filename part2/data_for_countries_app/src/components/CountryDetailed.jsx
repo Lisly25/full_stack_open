2712f3d23c069aa1b07009dbced4c73567b2fbab
Szabina Korbai <korbai.szabina@gmail.com>
2024-07-26 08:53:47 +0300

    Languages are now also displayed in one-country view

--- part2/data_for_countries_app/src/components/CountryDetailed.jsx
+++ part2/data_for_countries_app/src/components/CountryDetailed.jsx
@@ -1,9 +1,17 @@
 const CountryDetailed = (props) => {
+	const languageValues = Object.values(props.country.languages)
 	return (
 		<div>
 			<h1>{props.country.name.common}</h1>
 			<p>capital {props.country.capital}</p>
 			<p>area {props.country.area}</p>
+			<h2>languages:</h2>
+				<ul>
+					{languageValues.map((language) => 
+					<li key={language}>
+						{language}
+					</li>)}
+				</ul>
 		</div>
 	)
 }
