96b6856eae2fb9fe2b519f1ec10726763c5040d2
Szabina Korbai <korbai.szabina@gmail.com>
2025-03-31 16:11:59 +0300

    Exercise 4.5 done

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -8,7 +8,26 @@ const totalLikes = (blogs) => {
   return total_likes
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0)
+    return {}
+  
+  let favorite = blogs[0]
+
+  blogs.forEach(element => {
+    if (element.likes > favorite.likes)
+      favorite = element
+  });
+
+  return favorite_blog = {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
