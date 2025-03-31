5742139da318b7350a77a49604cb3bec800ce095
Szabina Korbai <korbai.szabina@gmail.com>
2025-03-31 15:50:57 +0300

    Exercise 4.4 done

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -3,6 +3,12 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const total_likes = blogs.reduce((accumulator, currentValue) => accumulator + currentValue.likes, 0)
+  return total_likes
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
