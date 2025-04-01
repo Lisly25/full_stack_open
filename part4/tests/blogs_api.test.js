62e285e21a969e2d4ebfd4d727d27c37138dee54
Szabina Korbai <korbai.szabina@gmail.com>
2025-04-01 16:14:19 +0300

    Exercise 4.10 done

--- part4/tests/blogs_api.test.js
+++ part4/tests/blogs_api.test.js
@@ -42,6 +42,30 @@ test("checking if the unique identifier is named 'id' and not '_id'", async () =
     assert.strictEqual(mongoose.isValidObjectId(response.body[0].id), true)
 })
 
+test("is it possible to add a new blog post", async () => {
+    const original_state = await api.get('/api/blogs')
+
+    const new_blog = {
+        author: "New blogger",
+        title: "Adding new blog",
+        url: "new_blog.com/new",
+        likes: 0
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(new_blog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const new_state = await api.get('/api/blogs')
+
+    const contents = new_state.body.map(response => response.title)
+
+    assert.strictEqual(original_state.body.length + 1, new_state.body.length)
+    assert(contents.includes('Adding new blog'))
+})
+
 after(async () => {
     await mongoose.connection.close()
 })
