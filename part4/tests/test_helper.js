4299de5860a28f4a00bd259fb6cc3287a33e1b1c
Szabina Korbai <korbai.szabina@gmail.com>
2025-04-01 13:37:35 +0300

    Exercise 4.8 done

---
+++ part4/tests/test_helper.js
@@ -0,0 +1,26 @@
+const Blog = require('../models/Blog')
+
+const initialBlogs = [
+    {
+        title: "First blog",
+        author: "John Doe",
+        url: "example.com/blog",
+        likes: 3
+    },
+    {
+        title: "Second blog",
+        author: "Jane Doe",
+        url: "foobar.com/blog",
+        likes: 30
+    }
+]
+
+const blogsInDB = async () => {
+    const blogs = await Blog.find({})
+    return blogs.map(blog => blog.toJSON())
+}
+
+module.exports = {
+    initialBlogs,
+    blogsInDB
+}
