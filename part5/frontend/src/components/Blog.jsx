21d1bcbd95ed5d347368f9d488b9523b3b66a826
Szabina Korbai <korbai.szabina@gmail.com>
2025-04-07 15:32:59 +0300

    Exercise 5.7 done

--- part5/frontend/src/components/Blog.jsx
+++ part5/frontend/src/components/Blog.jsx
@@ -1,7 +1,40 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [visibility, setVisibility] = useState(false)
+
+  const toggleVisibility = () => {
+    visibility ? setVisibility(false) : setVisibility(true)
+  }
+
+  if (!visibility)
+  {
+    return (
+      <div style={blogStyle}>
+        {blog.title} {blog.author} <button onClick={toggleVisibility}>view</button>
+      </div> 
+    )
+  }
+  else
+  {
+    return (
+      <div style={blogStyle}>
+        {blog.title} {blog.author} <button onClick={toggleVisibility}>hide</button><br/>
+        {blog.url}<br/>
+        {blog.likes}<button>like</button><br/>
+        {blog.user.username}<br/>
+      </div>  
+    )
+  }
+}
 
 export default Blog
