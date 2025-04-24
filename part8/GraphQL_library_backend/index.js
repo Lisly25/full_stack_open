a8485720b333d270aec3a51c04e0f765bd6c3575
Szabina Korbai <korbai.szabina@gmail.com>
2025-04-24 09:59:14 +0300

    Exercise 8.2 done

--- part8/GraphQL_library_backend/index.js
+++ part8/GraphQL_library_backend/index.js
@@ -98,18 +98,27 @@ let books = [
 */
 
 const typeDefs = `
+
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]!
+  }
+
   type Query {
-    dummy: Int
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `;
 
 const resolvers = {
   Query: {
-    dummy: () => 0,
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: () => books,
   },
 };
 
