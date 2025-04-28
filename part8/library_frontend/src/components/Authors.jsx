90f32f166ae3c1e5d6354ea2e079dcdf6fe61cef
Szabina Korbai <korbai.szabina@gmail.com>
2025-04-28 10:53:22 +0300

    Exercise 8.11 done

--- part8/library_frontend/src/components/Authors.jsx
+++ part8/library_frontend/src/components/Authors.jsx
@@ -1,5 +1,65 @@
-import { ALL_AUTHORS } from "../queries";
-import { useQuery } from "@apollo/client";
+import { ALL_AUTHORS, UPDATE_BIRTHYEAR } from "../queries";
+import { useQuery, useMutation } from "@apollo/client";
+import { useState, useEffect } from "react";
+
+const SetAuthorBirthYear = () => {
+  const [name, setName] = useState("");
+  const [year, setYear] = useState("");
+  const [error, setError] = useState("");
+
+  const [updateBirthYear, result] = useMutation(UPDATE_BIRTHYEAR, {
+    refetchQueries: [{ query: ALL_AUTHORS }],
+  });
+
+  useEffect(() => {
+    if (result.data && result.data.editAuthor === null) {
+      console.log("Failed to update author birthyear");
+      setError("Failed to update author birthyear");
+      setTimeout(() => setError(""), 5000);
+    }
+  }, [result.data]);
+
+  const handleYearUpdate = async (event) => {
+    event.preventDefault();
+
+    updateBirthYear({
+      variables: { name, setBornTo: Number(year) },
+    });
+
+    setName("");
+    setYear("");
+  };
+
+  const errorStyle = {
+    color: "red",
+    padding: 10,
+  };
+
+  return (
+    <div>
+      <div style={errorStyle}>{error}</div>
+      <h3>Set birthyear</h3>
+      <form onSubmit={handleYearUpdate}>
+        <div>
+          name
+          <input
+            value={name}
+            onChange={({ target }) => setName(target.value)}
+          />
+        </div>
+        <div>
+          born
+          <input
+            type="number"
+            value={year}
+            onChange={({ target }) => setYear(target.value)}
+          />
+        </div>
+        <button type="submit">update author</button>
+      </form>
+    </div>
+  );
+};
 
 const Authors = (props) => {
   const authors = useQuery(ALL_AUTHORS);
@@ -28,6 +88,7 @@ const Authors = (props) => {
             ))}
           </tbody>
         </table>
+        <SetAuthorBirthYear />
       </div>
     );
   }
--- part8/library_frontend/src/queries.js
+++ part8/library_frontend/src/queries.js
@@ -40,3 +40,14 @@ export const CREATE_BOOK = gql`
     }
   }
 `;
+
+export const UPDATE_BIRTHYEAR = gql`
+  mutation updateBirthyear($name: String!, $setBornTo: Int!) {
+    editAuthor(name: $name, setBornTo: $setBornTo) {
+      name
+      born
+      bookCount
+      id
+    }
+  }
+`;
